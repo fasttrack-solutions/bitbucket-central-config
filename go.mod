9aa3ae81134b8cc338e725896f3b7bffe9ec2d5b
Patrik Potocki <rambo3swe@gmail.com>
2020-05-05 11:01:27 +0200

    started

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/fasttrack-solutions/bitbucket-central-config
+
+go 1.13
---
+++ main.go
@@ -0,0 +1,8 @@
+package main
+
+import "log"
+
+func main() {
+	log.Print("Starting bitubkcet central config")
+
+}
