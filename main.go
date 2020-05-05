5519c9fe19fc5ee701f540b1ee3c6f7bb1bccaf7
Patrik Potocki <rambo3swe@gmail.com>
2020-05-05 12:59:59 +0200

    added support for default reviewers

--- main.go
+++ main.go
@@ -34,9 +34,47 @@ func main() {
 
 	log.Print(len(members.Values))
 
+	repos := []BitbucketRepository{}
+
+	page := 1
 	// Get repos
+	for {
+
+		resp, err = client.R().
+			SetResult(&BitbucketRepositoriesResponse{}).
+			Get(fmt.Sprintf("/repositories/%s?page=%d", workspace, page))
+
+		if err != nil {
+			log.Fatalf("Couldnt do request err: %v", err)
+		}
+
+		reposResp := resp.Result().(*BitbucketRepositoriesResponse)
+
+		if reposResp.Size == 0 {
+			break
+		}
+
+		for _, r := range reposResp.Values {
+			repos = append(repos, r)
+		}
+
+		page++
+	}
 
 	// Set default reviewers
+
+	for _, r := range repos {
+		for _, m := range members.Values {
+			resp, err = client.R().
+				SetResult(&BitbucketRepositoriesResponse{}).
+				Put(fmt.Sprintf("/repositories/%s/%s/default-reviewers/%s", workspace, r.Slug, m.User.UUID))
+
+			if err != nil {
+				log.Fatalf("Couldnt do request err: %v", err)
+			}
+			log.Printf("Added default reviewers for %s", r.Slug)
+		}
+	}
 
 	// Set Branching Model
 
@@ -52,13 +90,13 @@ type BitbucketMembersResponse struct {
 }
 
 type BitbucketMember struct {
-	Links     ValueLinks     `json:"links"`
-	Type      string         `json:"type"`
-	User      UserClass      `json:"user"`
-	Workspace WorkspaceClass `json:"workspace"`
+	Links     BitbucketMemberValueLinks `json:"links"`
+	Type      string                    `json:"type"`
+	User      UserClass                 `json:"user"`
+	Workspace WorkspaceClass            `json:"workspace"`
 }
 
-type ValueLinks struct {
+type BitbucketMemberValueLinks struct {
 	Self Self `json:"self"`
 }
 
@@ -87,4 +125,89 @@ type WorkspaceClass struct {
 	Name  string    `json:"name"`
 	Links UserLinks `json:"links"`
 	UUID  string    `json:"uuid"`
+}
+
+type BitbucketRepositoriesResponse struct {
+	Pagelen int64                 `json:"pagelen"`
+	Size    int64                 `json:"size"`
+	Values  []BitbucketRepository `json:"values"`
+	Page    int64                 `json:"page"`
+	Next    string                `json:"next"`
+}
+
+type BitbucketRepository struct {
+	SCM         string                        `json:"scm"`
+	Website     string                        `json:"website"`
+	HasWiki     bool                          `json:"has_wiki"`
+	UUID        string                        `json:"uuid"`
+	Links       BitbucketRepositoryValueLinks `json:"links"`
+	ForkPolicy  string                        `json:"fork_policy"`
+	FullName    string                        `json:"full_name"`
+	Name        string                        `json:"name"`
+	Project     ProjectClass                  `json:"project"`
+	Language    string                        `json:"language"`
+	CreatedOn   string                        `json:"created_on"`
+	Mainbranch  *Mainbranch                   `json:"mainbranch"`
+	Workspace   ProjectClass                  `json:"workspace"`
+	HasIssues   bool                          `json:"has_issues"`
+	Owner       Owner                         `json:"owner"`
+	UpdatedOn   string                        `json:"updated_on"`
+	Size        int64                         `json:"size"`
+	Type        string                        `json:"type"`
+	Slug        string                        `json:"slug"`
+	IsPrivate   bool                          `json:"is_private"`
+	Description string                        `json:"description"`
+}
+
+type BitbucketRepositoryValueLinks struct {
+	Watchers     Avatar  `json:"watchers"`
+	Branches     Avatar  `json:"branches"`
+	Tags         Avatar  `json:"tags"`
+	Commits      Avatar  `json:"commits"`
+	Clone        []Clone `json:"clone"`
+	Self         Avatar  `json:"self"`
+	Source       Avatar  `json:"source"`
+	HTML         Avatar  `json:"html"`
+	Avatar       Avatar  `json:"avatar"`
+	Hooks        Avatar  `json:"hooks"`
+	Forks        Avatar  `json:"forks"`
+	Downloads    Avatar  `json:"downloads"`
+	Pullrequests Avatar  `json:"pullrequests"`
+}
+
+type Avatar struct {
+	Href string `json:"href"`
+}
+
+type Clone struct {
+	Href string `json:"href"`
+	Name string `json:"name"`
+}
+
+type Mainbranch struct {
+	Type string `json:"type"`
+	Name string `json:"name"`
+}
+
+type Owner struct {
+	Username    string     `json:"username"`
+	DisplayName string     `json:"display_name"`
+	Type        string     `json:"type"`
+	UUID        string     `json:"uuid"`
+	Links       OwnerLinks `json:"links"`
+}
+
+type OwnerLinks struct {
+	Self   Avatar `json:"self"`
+	HTML   Avatar `json:"html"`
+	Avatar Avatar `json:"avatar"`
+}
+
+type ProjectClass struct {
+	Links OwnerLinks `json:"links"`
+	Type  string     `json:"type"`
+	UUID  string     `json:"uuid"`
+	Key   *string    `json:"key,omitempty"`
+	Name  string     `json:"name"`
+	Slug  *string    `json:"slug,omitempty"`
 }
